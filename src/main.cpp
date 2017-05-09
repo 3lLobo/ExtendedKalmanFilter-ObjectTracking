4becdd18a6eebf3ba893dcd1f786209160d42c10
3lLobo <flocwolf@gmail.com>
2017-05-09 17:56:57 +1000

    debugging
    debugged after review

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
 #include "Eigen/Dense"
-#include "FusionEKF.h"
+#include "ukf.h"
 #include "ground_truth_package.h"
 #include "measurement_package.h"
 
@@ -61,6 +62,10 @@ int main(int argc, char* argv[]) {
 
   check_files(in_file_, in_file_name_, out_file_, out_file_name_);
 
+  /**********************************************
+   *  Set Measurements                          *
+   **********************************************/
+
   vector<MeasurementPackage> measurement_pack_list;
   vector<GroundTruthPackage> gt_pack_list;
 
@@ -69,7 +74,6 @@ int main(int argc, char* argv[]) {
   // prep the measurement packages (each line represents a measurement at a
   // timestamp)
   while (getline(in_file_, line)) {
-
     string sensor_type;
     MeasurementPackage meas_package;
     GroundTruthPackage gt_package;
@@ -78,22 +82,23 @@ int main(int argc, char* argv[]) {
 
     // reads first element from the current line
     iss >> sensor_type;
+
     if (sensor_type.compare("L") == 0) {
-      // LASER MEASUREMENT
+      // laser measurement
 
       // read measurements at this timestamp
       meas_package.sensor_type_ = MeasurementPackage::LASER;
       meas_package.raw_measurements_ = VectorXd(2);
-      float x;
-      float y;
-      iss >> x;
-      iss >> y;
-      meas_package.raw_measurements_ << x, y;
+      float px;
+      float py;
+      iss >> px;
+      iss >> py;
+      meas_package.raw_measurements_ << px, py;
       iss >> timestamp;
       meas_package.timestamp_ = timestamp;
       measurement_pack_list.push_back(meas_package);
     } else if (sensor_type.compare("R") == 0) {
-      // RADAR MEASUREMENT
+      // radar measurement
 
       // read measurements at this timestamp
       meas_package.sensor_type_ = MeasurementPackage::RADAR;
@@ -110,66 +115,113 @@ int main(int argc, char* argv[]) {
       measurement_pack_list.push_back(meas_package);
     }
 
-    // read ground truth data to compare later
-    float x_gt;
-    float y_gt;
-    float vx_gt;
-    float vy_gt;
-    iss >> x_gt;
-    iss >> y_gt;
-    iss >> vx_gt;
-    iss >> vy_gt;
-    gt_package.gt_values_ = VectorXd(4);
-    gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
-    gt_pack_list.push_back(gt_package);
+      // read ground truth data to compare later
+      float x_gt;
+      float y_gt;
+      float vx_gt;
+      float vy_gt;
+      iss >> x_gt;
+      iss >> y_gt;
+      iss >> vx_gt;
+      iss >> vy_gt;
+      gt_package.gt_values_ = VectorXd(4);
+      gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
+      gt_pack_list.push_back(gt_package);
   }
 
-  // Create a Fusion EKF instance
-  FusionEKF fusionEKF;
+  // Create a UKF instance
+  UKF ukf;
 
   // used to compute the RMSE later
   vector<VectorXd> estimations;
   vector<VectorXd> ground_truth;
 
-  //Call the EKF-based fusion
-  size_t N = measurement_pack_list.size();
-  for (size_t k = 0; k < N; ++k) {
-    // start filtering from the second frame (the speed is unknown in the first
-    // frame)
-    fusionEKF.ProcessMeasurement(measurement_pack_list[k]);
+  // start filtering from the second frame (the speed is unknown in the first
+  // frame)
 
-    // output the estimation
-    out_file_ << fusionEKF.ekf_.x_(0) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(1) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(2) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(3) << "\t";
+  size_t number_of_measurements = measurement_pack_list.size();
 
-    // output the measurements
+  // column names for output file
+  out_file_ << "time_stamp" << "\t";  
+  out_file_ << "px_state" << "\t";
+  out_file_ << "py_state" << "\t";
+  out_file_ << "v_state" << "\t";
+  out_file_ << "yaw_angle_state" << "\t";
+  out_file_ << "yaw_rate_state" << "\t";
+  out_file_ << "sensor_type" << "\t";
+  out_file_ << "NIS" << "\t";  
+  out_file_ << "px_measured" << "\t";
+  out_file_ << "py_measured" << "\t";
+  out_file_ << "px_ground_truth" << "\t";
+  out_file_ << "py_ground_truth" << "\t";
+  out_file_ << "vx_ground_truth" << "\t";
+  out_file_ << "vy_ground_truth" << "\n";
+
+
+  for (size_t k = 0; k < number_of_measurements; ++k) {
+    // Call the UKF-based fusion
+    ukf.ProcessMeasurement(measurement_pack_list[k]);
+
+    // timestamp
+    out_file_ << measurement_pack_list[k].timestamp_ << "\t"; // pos1 - est
+
+    // output the state vector
+    out_file_ << ukf.x_(0) << "\t"; // pos1 - est
+    out_file_ << ukf.x_(1) << "\t"; // pos2 - est
+    out_file_ << ukf.x_(2) << "\t"; // vel_abs -est
+    out_file_ << ukf.x_(3) << "\t"; // yaw_angle -est
+    out_file_ << ukf.x_(4) << "\t"; // yaw_rate -est
+
+    // output lidar and radar specific data
     if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER) {
-      // output the estimation
+      // sensor type
+      out_file_ << "lidar" << "\t";
+
+      // NIS value
+      out_file_ << ukf.NIS_laser_ << "\t";
+
+      // output the lidar sensor measurement px and py
       out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
       out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
+
     } else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR) {
-      // output the estimation in the cartesian coordinates
+      // sensor type
+      out_file_ << "radar" << "\t";
+
+      // NIS value
+      out_file_ << ukf.NIS_radar_ << "\t";
+
+      // output radar measurement in cartesian coordinates
       float ro = measurement_pack_list[k].raw_measurements_(0);
       float phi = measurement_pack_list[k].raw_measurements_(1);
-      out_file_ << ro * cos(phi) << "\t"; // p1_meas
-      out_file_ << ro * sin(phi) << "\t"; // ps_meas
+      out_file_ << ro * cos(phi) << "\t"; // px measurement
+      out_file_ << ro * sin(phi) << "\t"; // py measurement
     }
 
-    // output the ground truth packages
+    // output the ground truth
     out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(3) << "\n";
 
-    estimations.push_back(fusionEKF.ekf_.x_);
+    // convert ukf x vector to cartesian to compare to ground truth
+    VectorXd ukf_x_cartesian_ = VectorXd(4);
+
+    float x_estimate_ = ukf.x_(0);
+    float y_estimate_ = ukf.x_(1);
+    float vx_estimate_ = ukf.x_(2) * cos(ukf.x_(3));
+    float vy_estimate_ = ukf.x_(2) * sin(ukf.x_(3));
+    
+    ukf_x_cartesian_ << x_estimate_, y_estimate_, vx_estimate_, vy_estimate_;
+    
+    estimations.push_back(ukf_x_cartesian_);
     ground_truth.push_back(gt_pack_list[k].gt_values_);
+
   }
 
   // compute the accuracy (RMSE)
   Tools tools;
-  cout << "Accuracy - RMSE:" << endl << tools.CalculateRMSE(estimations, ground_truth) << endl;
+  cout << "RMSE" << endl << tools.CalculateRMSE(estimations, ground_truth) << endl;
 
   // close files
   if (out_file_.is_open()) {
@@ -180,5 +232,6 @@ int main(int argc, char* argv[]) {
     in_file_.close();
   }
 
+  cout << "Done!" << endl;
   return 0;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,56 +15,17 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-  // now calculate the RMSE
-  VectorXd rmse(4);
-  rmse << 0, 0, 0, 0;
-  
-  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
-    cout << "Invalid estimation or ground_truth data" << endl;
-    return rmse;
-  }
+	VectorXd rmse(4);
+	rmse << 0,0,0,0; 
 
-  for(unsigned int i =0; i< estimations.size(); i++){
-    VectorXd residual = estimations[i] - ground_truth[i];
-
-    residual = residual.array() * residual.array();
-    rmse += residual;
-  }
-	//calculate the mean
-  rmse = rmse / estimations.size();
-  //take the square root
-  rmse = rmse.array().sqrt();
-
-  return rmse;
-}
-
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
-	//define Jacobian Matrix
-	MatrixXd Hj(3,4);
+	for(int i=0; i < estimations.size(); ++i){
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}		
+	// Mean of rmse and squareroot
+    rmse = rmse/estimations.size();
+	rmse = sqrt(rmse.array());
 	
-	float px = x_state(0);
-	float py = x_state(1);
-	float vx = x_state(2);
-	float vy = x_state(3);
-
-	// more often used terms
-	float t_1 = px * px + py * py;
-	float t_2 = sqrt(t_1);
-	float t_3 = (t_1 * t_2);
-
-	// error if devided by zero
-	if(fabs(t_1) < 0.0001){
-		cout << "Error! /n Division by Zero while calculating Jacobian matrix" << endl;
-		return Hj;
-	}
-
-	// calculate Jacobian matrix
-	Hj << (px / t_2), (py / t_2), 0, 0, -(py / t_1), (px / t_1), 0, 0, py * (vx * py - vy * px) / t_3, px * (px * vy - py * vx) / t_3, px / t_2, py / t_2;
-
-	return Hj;
-  
+	return rmse
 }
